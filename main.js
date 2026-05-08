108e9a0c45a8a2650c0727767267ed99f85f3eac
portable <heycallieweb@hotmail.com>
2026-05-08 12:10:07 -0400

    i don't remember

---
+++ main.js
@@ -0,0 +1,20 @@
+function mobileCheck() {
+  return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
+    navigator.userAgent,
+  );
+}
+//      if (mobileCheck() === false) {
+//        window.location = "https://heycallie.net";
+//      }
+
+function checkThatItWorks() {
+  console.log("Open recieved");
+}
+
+function openApp(id) {
+  document.getElementById(id).classList.remove("hidden");
+}
+
+function closeApp(id) {
+  document.getElementById(id).classList.add("hidden");
+}
